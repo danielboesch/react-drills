6e1255908497946a77884a8742c29237f747fd9a
Daniel Boesch <you@example.com>
2021-04-26 14:19:33 -0600

    app-4 done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Image picture = {'https://images.unsplash.com/photo-1619017236031-6f55d516f49a?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ2fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'}/>
+  
     </div>
   );
 }
